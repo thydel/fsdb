50f6571771e714a41c9b617efceec5bcf776823c
Thierry Delamare <t.delamare@laposte.net>
2026-05-28 00:13:27 +0200

    Uses prettier instead of pandoc

--- skills/md-formatter/scripts/fmt-md.sh
+++ skills/md-formatter/scripts/fmt-md.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 for file in "$@"; do
-  pandoc -f gfm -t gfm --wrap=auto --columns=80 "$file" -o "${file}.tmp" && mv "${file}.tmp" "$file"
+  # pandoc -f gfm -t gfm --wrap=auto --columns=80 "$file" -o "${file}.tmp" && mv "${file}.tmp" "$file"
+  npx prettier --prose-wrap always --print-width 80 --write "$file"
 done
